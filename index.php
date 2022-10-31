63b81582149337214b1945e7b5fd13659f7fb0f5
nathan-leeman <45040617+nathan-leeman@users.noreply.github.com>
2022-10-31 15:35:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hello World - FD Standard"
+echo "Hello World - FD Classic FD Classic  FD Classic FD Classic FD ClassicFD Classic FD Classic FD Classicv FD Classic FD Classic FD ClassicFD Classic FD Classic"
 
 ?>
